413184638bb4fe7f94f4c27213fb847db6fdbc04
danykim57 <55152550+danykim57@users.noreply.github.com>
2022-01-23 17:44:29 +0900

    Delete ModernJava directory

--- ModernJava/src/main/java/com/example/modernjava/MyAbstractClass.java
+++
@@ -1,8 +0,0 @@
-package com.example.modernjava;
-
-public abstract class MyAbstractClass {
-    public abstract void meh();
-    public void sayBoy() {
-        System.out.println("Boi! ");
-    }
-}
