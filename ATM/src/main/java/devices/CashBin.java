75027671c7430f6a259c538a0dd92179eb0449c8
danykim57 <55152550+danykim57@users.noreply.github.com>
2022-04-13 21:32:48 +0900

    Delete ATM directory

--- ATM/src/main/java/devices/CashBin.java
+++
@@ -1,7 +0,0 @@
-package devices;
-
-public interface CashBin {
-    boolean checkStoredAmount(int amount); //Check cash bin space to put in money;
-
-    void performDeposit(); //Physically putting in money to cashbin
-}
